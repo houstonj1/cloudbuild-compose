7b88d61ac73200c11d6c1954fc8753a85f34eaa1
James Houston <james-houston@pluralsight.com>
2020-11-02 12:32:50 -0500

    Use docker hostname for postgres

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg');
 const client = new Client({
-    host: 'localhost',
+    host: process.env.PG_HOST || 'localhost',
     user: 'james',
     password: 'james',
     database: 'james'
